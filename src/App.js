5a1d5dccaa2850a0c6cfdadc80f20bf64ee4a507
Zainul Saifulah Aziz <zainulsaifulahaziz@gmail.com>
2021-01-25 09:58:23 +0700

    add news layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+import Home from './Home';
+import Content from './Content';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  state = {
+    query: ""
+  }
+
+  search(event){
+    console.log(event.target.value)
+
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/content">Content</Nav.Link>
+            </Nav>
+            <Form inline onSubmit={() => this.setState({query: this.state.input})}>
+              <FormControl type="text" value={this.state.input} onChange={ event => this.setState({input: event.target.value})} placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success" onClick={() => this.setState({query: this.state.input}) }>Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <div>
+          <Switch>
+            <Route path="/content/:country" children={<Child />}/>
+            <Route path="/content">
+              <Content query = {this.state.query} />
+            </Route>
+            <Route path="/">
+              <Home query = {this.state.query} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
+
+function Child() {
+  let { country } = useParams()
+
+  return (
+    <Content country = {country} />
+  )
+}
+
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Card, Row, Col, Container } from 'react-bootstrap';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://newsapi.org/v2/top-headlines?country=id&apiKey=2e2e276acd444b4cacce4769c94604b9')
+            .then(news => news.json())
+            .then(json => this.setState({ news: json.articles }))
+    }
+
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.query !== prevProps.query) {
+            fetch(`https://newsapi.org/v2/everything?q=${this.props.query}&apiKey=2e2e276acd444b4cacce4769c94604b9`)
+            .then(news => news.json())
+            .then(json => this.setState({ news: json.articles }))
+        }
+    }
+    
+
+    loopRender() {
+        let row = []
+
+        if (this.state.news.length !== 0) {
+            this.state.news.map(article => {
+                row.push(
+                    <Col md={3}>
+                        <Card>
+                        <Card.Img width="auto" height="160px" variant="top" src={article.urlToImage} />
+                        <Card.Body>
+                            <Card.Title>{article.title}</Card.Title>
+                            <Card.Text>
+                            {article.description}
+                            </Card.Text>
+                            <Card.Link href={article.url} target="_blank">Detail</Card.Link>
+                        </Card.Body>
+                        </Card>
+                    </Col>
+                )
+            })
+        }
+
+        return row
+    }
+
+
+    
+    render() {
+        return (
+            <Container>
+                <Row>
+                    {this.loopRender()}
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Home
